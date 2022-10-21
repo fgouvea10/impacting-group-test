d2710f9142d73c63e487bdaed0df0f9ac2d61832
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-21 17:00:25 -0300

    refactor: create pages repository

--- backend/src/routes/posts.routes.ts
+++ backend/src/routes/posts.routes.ts
@@ -1,56 +1,10 @@
 import { Router } from "express";
+
 import { PostsRepository } from "../repositories/posts-repository";
 
 const postsRepository = new PostsRepository();
 
-// type Post = {
-//   title: string;
-//   category: string;
-//   content: string;
-// };
-
 export const postsRoutes = Router();
-
-// const posts: Post[] = [
-//   {
-//     title: "O Papel do novo centro da juventude para todos",
-//     category: "messages",
-//     content: "...",
-//   },
-//   { title: "Mensagem de Natal 2020", category: "messages", content: "..." },
-//   {
-//     title: "Obras na avenida 25 de Abril finalizadas atempadamente",
-//     category: "news",
-//     content: "...",
-//   },
-//   {
-//     title: "Piscinas municipais abrem portas as cidadãos carenciados",
-//     category: "news",
-//     content: "...",
-//   },
-//   {
-//     title: "Horários dos serviços municipais",
-//     category: "schedules",
-//     content: "...",
-//   },
-//   {
-//     title: "Horários das instalações desportivas",
-//     category: "schedules",
-//     content: "...",
-//   },
-//   {
-//     title: "Atendimento ao cliente no serviço de águas",
-//     category: "schedules",
-//     content: "...",
-//   },
-//   { title: "Tarifas de licenciamento", category: "taxes", content: "..." },
-//   {
-//     title: "Tarifários de água do concelho",
-//     category: "taxes",
-//     content: "...",
-//   },
-//   { title: "Taxas de saneamento", category: "taxes", content: "..." },
-// ];
 
 postsRoutes.post("/posts", (request, response) => {
   const { title, category, content } = request.body;
@@ -73,7 +27,3 @@ postsRoutes.get("/posts", (request, response) => {
 
   return response.json(posts);
 });
-
-// postsRoutes.get("/posts", (request, response) => {
-//   return response.status(200).json(posts);
-// });
