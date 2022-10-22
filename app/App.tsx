fb4956ff4b841e2e0f171b67ef2842fc77a10a7e
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-22 19:11:03 -0300

    feat: add correct import order

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,11 @@
 import AppLoading from "expo-app-loading";
 import { NavigationContainer } from "@react-navigation/native";
 import { Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import { useFetch } from "./src/hooks/useFetch";
 import { PageProvider } from "./src/contexts/PageContext";
 import { AppRoutes } from "./src/routes/app.routes";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const { isFetching } = useFetch("/pages");
