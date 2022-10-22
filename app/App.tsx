dc3a870272e943466a3965606d69efa3d76fd373
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-22 19:01:38 -0300

    feat: add custom status bar background

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,23 @@
 import AppLoading from "expo-app-loading";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import { Platform } from "react-native";
 
 import { useFetch } from "./src/hooks/useFetch";
-import { PageProvider } from './src/contexts/PageContext';
+import { PageProvider } from "./src/contexts/PageContext";
 import { AppRoutes } from "./src/routes/app.routes";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  const { isFetching } = useFetch('/pages');
+  const { isFetching } = useFetch("/pages");
 
   if (isFetching) return <AppLoading />;
 
   return (
     <NavigationContainer>
       <PageProvider>
+        <StatusBar
+          backgroundColor={Platform.OS === "ios" && "#000000"}
+        />
         <AppRoutes />
       </PageProvider>
     </NavigationContainer>
